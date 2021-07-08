1d504cbc89e652992418ffc14809c3230d838942
trapintrovert <mcneo062@gmail.com>
2021-07-08 18:46:12 +0100

    Sell Order Page Done

--- src/components/New/SellOrder.js
+++ src/components/New/SellOrder.js
@@ -71,7 +71,7 @@ const SellOrder = () => {
   const currentPosts = filterCoin.slice(indexOfFirstPost, indexOfLastPost);
   
   
-  const coinType = (coin_type) => {
+const coinType = (coin_type) => {
     if(coin_type === 'USDT_ERC_20'){
       return "USDT (ERC20)";
     }else if(coin_type === 'ETH_BEP_20'){
@@ -85,197 +85,93 @@ const SellOrder = () => {
     }else {
       return coin_type
     }
-  }
+}
+
+const coinImage = (coin_type) => {
+    if(coin_type === 'USDT_ERC_20' || coin_type === 'USDT_BEP_20' || coin_type === 'USDT_TRON'){
+      return <img src={USDT} alt={coin_type}/> ;
+    }else if(coin_type === 'DOGE'){
+        return <img src={DOGE} alt={coin_type}/>;
+    }else if(coin_type === 'BTC' || coin_type === 'BTC_BEP_20'){
+      return  <img src={Bitcoin} alt={coin_type}/>;
+    }else if(coin_type === 'ETH' || coin_type === 'ETH_BEP_20'){
+       return  <img src={ETH} alt={coin_type}/>;
+    }else if(coin_type === 'LTC'){
+      return <img src={LTC} alt={coin_type}/>;
+    }else if(coin_type === 'NGNT'){
+        return <img src={NGNT} alt={coin_type}/>;
+    }else if(coin_type === 'BNB'){
+        return <img src={BNB} alt={coin_type}/>;
+    }else if(coin_type === 'BUSD'){
+        return <img src={BUSD} alt={coin_type}/>;
+    }else{
+      return coin_type
+    }
+}
 
 
     return (
         <div className="scalex-order__scalex-sell-order">
-            <Row>
-                <Col md={{ size: 6, offset: 3 }} className="mb-3">
-                    <Input 
-                    className="mt-5"
-                    type='search'
-                    placeholder='search coin. Eg. BTC'
-                    onChange={onSearchChange}
-                    />
-                </Col>
-            </Row>
-           <Row>
-           {
-            currentPosts.map((offer, i) => (
-                <Col lg={4} md={6} sm={12}>
-                    <Card>
-                        <div className="d-flex  align-items-center pb-3">
-                            <img src={Bitcoin} alt="Ngnt"/> 
-                            <h3>{coinType(offer.coin_type)}</h3>
-                        </div>
-                        <div className="d-flex justify-content-between py-1">
-                            <h6>Quantity: <span>{offer.available_amount}</span></h6>
-                            <h6>Negotiable: <span> {offer.negotiable === true ? ('Yes') : ('No')}</span></h6>
-                        </div>
-                        <div className="d-flex justify-content-between py-1">
-                            <h6>Price(₦): <span>{offer.rate_in_fiat.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')} </span></h6>
-                            <h6>Min Payment: <span> ₦{offer.minimum_limit.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')} </span></h6>
-                        </div>
-                        <a href={`https://t.me/ScaleXP2PBot?start=q_${offer.transaction_id}`} >
-                            <Button color="danger" className="btn-block ">Sell</Button>
-                        </a>
-                    </Card>
-                </Col>
-              
-            ))
-          }
-                
-                {/*<Col lg={4} md={6} sm={12}>
-                    <Card>
-                        <div className="d-flex  align-items-center pb-3">
-                            <img src={Bitcoin} alt="Ngnt"/> 
-                            <h3>Bitcoin</h3>
-                        </div>
-                        <div className="d-flex justify-content-between py-1">
-                            <h6>Quantity: <span>#450</span></h6>
-                            <h6>Negotiable: <span> YES</span></h6>
-                        </div>
-                        <div className="d-flex justify-content-between py-1">
-                            <h6>Price: <span>#450</span></h6>
-                            <h6>Min Payment: <span> #10, 000</span></h6>
-                        </div>
-                        <a href="https://t.me/scalexp2pbot" >
-                            <Button color="danger" className="btn-block ">Sell</Button>
-                        </a>
-                    </Card>
-                </Col>
-                <Col lg={4} md={6} sm={12}>
-                    <Card>
-                        <div className="d-flex  align-items-center pb-3">
-                            <img src={USDT} alt="Ngnt"/> 
-                            <h3>USDT</h3>
-                        </div>
-                        <div className="d-flex justify-content-between py-1">
-                            <h6>Quantity: <span>#450</span></h6>
-                            <h6>Negotiable: <span> YES</span></h6>
-                        </div>
-                        <div className="d-flex justify-content-between py-1">
-                            <h6>Price: <span>#450</span></h6>
-                            <h6>Min Payment: <span> #10, 000</span></h6>
-                        </div>
-                        <a href="https://t.me/scalexp2pbot" >
-                            <Button color="danger" className="btn-block ">Sell</Button>
-                        </a>
-                    </Card>
-                </Col>
-                <Col lg={4} md={6} sm={12}>
-                    <Card>
-                        <div className="d-flex  align-items-center pb-3">
-                            <img src={ETH} alt="Ngnt"/> 
-                            <h3>Ethereum</h3>
-                        </div>
-                        <div className="d-flex justify-content-between py-1">
-                            <h6>Quantity: <span>#450</span></h6>
-                            <h6>Negotiable: <span> YES</span></h6>
-                        </div>
-                        <div className="d-flex justify-content-between py-1">
-                            <h6>Price: <span>#450</span></h6>
-                            <h6>Min Payment: <span> #10, 000</span></h6>
-                        </div>
-                        <a href="https://t.me/scalexp2pbot" >
-                            <Button color="danger" className="btn-block ">Sell</Button>
-                        </a>
-                    </Card>
-                </Col>
-                <Col lg={4} md={6} sm={12}>
-                    <Card>
-                        <div className="d-flex  align-items-center pb-3">
-                            <img src={DOGE} alt="Ngnt"/> 
-                            <h3>DOGE</h3>
-                        </div>
-                        <div className="d-flex justify-content-between py-1">
-                            <h6>Quantity: <span>#450</span></h6>
-                            <h6>Negotiable: <span> YES</span></h6>
-                        </div>
-                        <div className="d-flex justify-content-between py-1">
-                            <h6>Price: <span>#450</span></h6>
-                            <h6>Min Payment: <span> #10, 000</span></h6>
-                        </div>
-                        <a href="https://t.me/scalexp2pbot" >
-                            <Button color="danger" className="btn-block ">Sell</Button>
-                        </a>
-                    </Card>
-                </Col>
-                <Col lg={4} md={6} sm={12}>
-                    <Card>
-                        <div className="d-flex  align-items-center pb-3">
-                            <img src={LTC} alt="Ngnt"/> 
-                            <h3>Litecoin</h3>
-                        </div>
-                        <div className="d-flex justify-content-between py-1">
-                            <h6>Quantity: <span>#450</span></h6>
-                            <h6>Negotiable: <span> YES</span></h6>
-                        </div>
-                        <div className="d-flex justify-content-between py-1">
-                            <h6>Price: <span>#450</span></h6>
-                            <h6>Min Payment: <span> #10, 000</span></h6>
-                        </div>
-                        <a href="https://t.me/scalexp2pbot" >
-                            <Button color="danger" className="btn-block ">Sell</Button>
-                        </a>
-                    </Card>
-                </Col>
-                <Col lg={4} md={6} sm={12}>
-                    <Card>
-                        <div className="d-flex  align-items-center pb-3">
-                            <img src={BNB} alt="Ngnt"/> 
-                            <h3>BNB</h3>
-                        </div>
-                        <div className="d-flex justify-content-between py-1">
-                            <h6>Quantity: <span>#450</span></h6>
-                            <h6>Negotiable: <span> YES</span></h6>
-                        </div>
-                        <div className="d-flex justify-content-between py-1">
-                            <h6>Price: <span>#450</span></h6>
-                            <h6>Min Payment: <span> #10, 000</span></h6>
-                        </div>
-                        <a href="https://t.me/scalexp2pbot" >
-                            <Button color="danger" className="btn-block ">Sell</Button>
-                        </a>
-                    </Card>
-                </Col>
-                <Col lg={4} md={6} sm={12}>
-                    <Card>
-                        <div className="d-flex  align-items-center pb-3">
-                            <img src={BUSD} alt="Ngnt"/> 
-                            <h3>BNB</h3>
-                        </div>
-                        <div className="d-flex justify-content-between py-1">
-                            <h6>Quantity: <span>#450</span></h6>
-                            <h6>Negotiable: <span> YES</span></h6>
-                        </div>
-                        <div className="d-flex justify-content-between py-1">
-                            <h6>Price: <span>#450</span></h6>
-                            <h6>Min Payment: <span> #10, 000</span></h6>
-                        </div>
-                        <a href="https://t.me/scalexp2pbot" >
-                            <Button color="danger" className="btn-block ">Sell</Button>
-                        </a>
-                    </Card>
-    </Col>*/}
-           </Row>
-           <Row>
-                <Col className="mt-3">
-                    {
-                    filterCoin.length > 0 ? (
-                        <OrderPagination
-                        postsPerPage={postsPerPage} 
-                        totalPosts={filterCoin.length} 
-                        paginate={paginate} 
-                        />
-                        
-                    ): (
-                        <p className="text-center">No trade available</p>
-                    )
-                    }
-                </Col>
-                </Row>
+            {
+                loading ? (
+                    <Spinner style={{ width: '3rem', height: '3rem', top: '50%', left: '47%', position: "absolute" }} />
+                ):(
+                    <Fragment>
+                        <Row>
+                            <Col md={{ size: 6, offset: 3 }} className="mb-3">
+                                <Input 
+                                    className="search mt-5"
+                                    type='search'
+                                    placeholder='search coin. Eg. BTC'
+                                    onChange={onSearchChange}
+                                />
+                            </Col>
+                        </Row>
+                        <Row>
+                        {
+                            currentPosts.map((offer, i) => (
+                                <Col lg={4} md={6} sm={12}>
+                                    <Card>
+                                        <div className="d-flex  align-items-center pb-3">
+                                            {coinImage(offer.coin_type)}
+                                            <h3>{coinType(offer.coin_type)}</h3>
+                                        </div>
+                                        <div className="d-flex justify-content-between py-1">
+                                            <h6>Quantity: <span>{offer.available_amount}</span></h6>
+                                            <h6>Negotiable: <span> {offer.negotiable === true ? ('Yes') : ('No')}</span></h6>
+                                        </div>
+                                        <div className="d-flex justify-content-between py-1">
+                                            <h6>Price(₦): <span>{offer.rate_in_fiat.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')} </span></h6>
+                                            <h6>Min Payment: <span> ₦{offer.minimum_limit.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')} </span></h6>
+                                        </div>
+                                        <a href={`https://t.me/ScaleXP2PBot?start=q_${offer.transaction_id}`} >
+                                            <Button color="danger" className="btn-block ">Sell</Button>
+                                        </a>
+                                    </Card>
+                                </Col>
+                            
+                            ))
+                        }   
+                        </Row>
+                        <Row>
+                            <Col className="mt-3">
+                                {
+                                    filterCoin.length > 0 ? (
+                                        <OrderPagination
+                                            postsPerPage={postsPerPage} 
+                                            totalPosts={filterCoin.length} 
+                                            paginate={paginate} 
+                                        />
+                                        
+                                    ):(
+                                        <p className="text-center text-black no-data">No trade available</p>
+                                    )
+                                }
+                            </Col>
+                        </Row>
+                    </Fragment>
+                )
+            }
         </div>
     )
 }
