44e919cc5501d619974ab6f3eb2d4b74ff5bec60
trapintrovert <mcneo062@gmail.com>
2021-07-15 10:44:43 +0100

    All old pages from previous design removed

--- src/App.js
+++ src/App.js
@@ -8,24 +8,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './scss/main.scss';
 
-import Home from "./pages/Home";
-import Home2 from './pages/New/Home';
+import Home from './pages/New/Home';
 import Verification from "./pages/New/Verification";
 import Converter from './pages/New/Converter'
-import Transactions from "./pages/Transactions";
-import Orders from "./pages/Orders";
-// import Converter from "./pages/Converter";
 import Order from "./pages/New/Order";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={Home2} />
-        <Route exact path="/old-home" component={Home} />
+        <Route exact path="/" component={Home} />
         <Route exact path="/verification/tier-2/:id" component={Verification} />
-        <Route exact path="/transactions/:id" component={Transactions}/>
-        <Route exact path="/old-orders" component={Orders}/>
         <Route exact path="/orders" component={Order}/>
         <Route exact path="/converter" component={Converter}/>
       </Switch>
