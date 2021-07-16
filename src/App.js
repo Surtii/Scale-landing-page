032d1e86307f26bb0b8a84fed3bc5af3dc52b894
trapintrovert <mcneo062@gmail.com>
2021-07-16 12:20:46 +0100

    Homepage fix

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import './styles/style.css'
-import './styles/color-one.css'
-import './styles/responsive.css'
-import './styles/color-one.css'
-import './styles/custom-animation.css'
+// import './styles/color-one.css'
+// import './styles/responsive.css'
+// import './styles/color-one.css'
+// import './styles/custom-animation.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'owl.carousel/dist/assets/owl.carousel.css'; 
 import 'owl.carousel/dist/assets/owl.theme.default.css';  
